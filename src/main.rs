cac54b053faddea19cbef8925a85d167a31993fa
David-McIlveen <115719576+David-McIlveen@users.noreply.github.com>
2023-11-10 11:42:39 -0600

    test

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,22 @@ fn main() {
     if args.len() >= 2 {
         let command = args[1].to_uppercase();
         match command.as_str() {
-            "CRV" => attempt_to_create_valid_file(args),
-            "VAL" => attempt_to_validate_file(args),
-            _ => println!("Command does not exist")
+            "CRV" => match attempt_to_create_valid_file(&args) {
+                Ok(s) => println!("{}", s),
+                Err(e) => println!("{}", e)
+            },
+            "RVF" => match attempt_to_restore_valid_file(&args) {
+                Ok(s) => println!("{}", s),
+                Err(e) => println!("{}", e)
+            },
+            "VAL" => match attempt_to_validate_file(&args) {
+                Ok(s) => println!("{}", s),
+                Err(e) => println!("{}", e)
+            },
+            _ => println!("Command: \"{}\" does not exist.", command)
         }
     } else {
-        println!("No command entered");
+        println!("No command entered.");
     }
     
     // match read_this_file("test.txt") {
@@ -47,7 +57,7 @@ fn main() {
     // }
 }
 
-fn attempt_to_create_valid_file(args:Vec<String>){
+fn attempt_to_create_valid_file(args:&Vec<String>) -> Result<&str, &str>{
     if args.len() >= 3 {
         let file_to_enter = args[2].clone();
         let file_to_save = {
@@ -55,35 +65,66 @@ fn attempt_to_create_valid_file(args:Vec<String>){
                 args[3].clone()
             } else {
                 let mut file_temp = file_to_enter.clone();
-                match file_temp.rfind(".") {
-                    Some(i) => file_temp.replace_range(i.., ""),
-                    None => ()
-                }
+                remove_file_extension(&mut file_temp);
                 file_temp.push_str(".enc");
                 file_temp
             }
         };
         match create_valid_file(&file_to_enter, &file_to_save) {
-            Ok(..) => println!("Validatable file created!"),
-            Err(..) => println!("Could not create validatable file!")
+            Ok(..) => return Ok("Validatable file created."),
+            Err(..) => return Err("Could not create validatable file.")
         }
     } else {
-        println!("No file entered!");
+        return Err("No file entered.");
     }
 }
 
-fn attempt_to_validate_file(args:Vec<String>){
+fn remove_file_extension(file_name:&mut String){
+    match file_name.rfind(".") {
+        Some(i) => file_name.replace_range(i.., ""),
+        None => ()
+    }
+}
+
+fn attempt_to_restore_valid_file(args:&Vec<String>) -> Result<&str, &str> {
+    if args.len() >= 3 {
+        let file_to_unvalidate = args[2].clone();
+        match validate_file(&file_to_unvalidate.as_str()) {
+            Ok(..) => (),
+            Err(..) => println!("WARNING! File being restored is not valid!")
+        }
+        let file_to_save_to = {
+            if args.len() >= 4  {
+                args[3].clone()
+            } else {
+                let mut temp_name: String = file_to_unvalidate.clone();
+                remove_file_extension(&mut temp_name);
+                temp_name.push_str(".temp");
+                println!("- WARNING! No file to write to specified, a defualt template will be used.\n- You will need to rename the file with the proper extension!");
+                temp_name
+            }
+        };
+        match restore_valid_file(&file_to_unvalidate.as_str(), &file_to_save_to.as_str()) {
+            Ok(..) => return Ok("Hashing data removed from file."),
+            Err(..) => return Err("Hashing data could not be removed.")
+        }
+    } else {
+        return Err("No file entered.");
+    }
+}
+
+fn attempt_to_validate_file(args:&Vec<String>)  -> Result<&str, &str>{
     if args.len() >= 3 {
         match validate_file(args[2].as_str()) {
             Ok(s) => if s {
-                println!("File was valid!");
+                return Ok("File was valid.");
             } else {
-                println!("File was invalid!");
+                return Ok("File was invalid.");
             },
-            Err(..) => println!("Could not validate file!")
+            Err(..) => return Err("Could not validate file.")
         }
     } else {
-        println!("No file entered!");
+        return Err("No file entered.");
     }
 }
 
@@ -144,6 +185,22 @@ fn create_valid_file(file_name:&str, new_file_name:&str) -> io::Result<()>{
     Ok(())
 }
 
+fn restore_valid_file(file_name:&str, new_file_name:&str) -> io::Result<()> {
+    let mut og_file = File::open(file_name)?;
+    let mut new_file = File::create(new_file_name)?;
+    let mut hash_buffer = [0; 64];
+    let mut data_buffer = [0; 4096];
+    og_file.read(&mut hash_buffer)?;
+    loop {
+        let bytes_read = og_file.read(&mut data_buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        new_file.write(&data_buffer[..bytes_read])?;
+    }
+    Ok(())
+}
+
 fn validate_file(file_name:&str) -> io::Result<bool> {
     let hash_bytes = read_first_bytes::<64>(file_name)?;
     let path = Path::new(&file_name);
@@ -152,7 +209,7 @@ fn validate_file(file_name:&str) -> io::Result<bool> {
         Some(x) => temp_file_name = format!("~{}", x.to_str().unwrap()),
         None => ()
     }
-    println!("{}", temp_file_name);
+    //println!("{}", temp_file_name);
     fs::copy(file_name, &temp_file_name)?;
     let mut temp_file = OpenOptions::new().write(true).open(temp_file_name.clone())?;
     let empty_buffer = [0; 64];
